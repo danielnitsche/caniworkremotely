a4a47119f39f8ff1f9aa7642b0096bcd70aea767
Daniel Nitsche <github@nitsche.id.au>
2014-02-15 13:51:59 +1100

    Initial styles

--- scripts/main.js
+++ scripts/main.js
@@ -8,12 +8,16 @@
 
 	$('.question').addEventListener('submit', function(e) {
 		e.preventDefault();
+		$('body').className = 'answering';
 		$('.answer').innerHTML = '';
 		var location = $('.location').value;
+		setTimeout(function() {
+			$('body').className += ' elapsed-1'
+		}, 1000);
 
 		getRemoteUTC(location, function(remoteUTC) {
 			if (!remoteUTC) {
-				display('<span class="yesorno">I’m not sure</span> <span class="because">I couldn’t work out where <em>' + location + '</em> is :(</span>');
+				display('<span class="yesorno">I’m not sure :(</span> <span class="because">I couldn’t work out where “' + location + '” is</span>');
 			} else {
 				if (remoteUTC == localUTC) {
 					display('<span class="yesorno">Yes</span> <span class="because">You’re in the same timezone as the company headquarters</span>');
@@ -34,7 +38,7 @@
 					} else if (overlap.hours < 4) {
 						if (overlap.earlyStartIncreasesOverlap) {
 							if (overlap.workOnSaturday) {
-								display('<span class="yesorno but">Yes</span> <span class="info">But you’ll need to work Tuesday to Saturday, and you’ll need to shift your working day a couple of hours earlier</span>');
+								display('<span class="yesorno but">Yes</span> <span class="info">if you work Tuesday to Saturday, and you shift your working day a couple of hours earlier</span>');
 							} else if (overlap.workOnSunday) {
 								display('<span class="yesorno but">Yes</span> <span class="info">But you’ll need to work Sunday to Thursday, and you’ll need to shift your working day a couple of hours earlier</span>');
 							} else {
@@ -71,7 +75,7 @@
 
 	function display(message) {
 		$('.answer').innerHTML = message;
-		$('.answer').className = 'answer on';
+		$('body').className += ' answered';
 	}
 
 	function getRemoteUTC(location, callback) {
